0c555f5e5e70ec10ab4c730f1145d27243056741
Peter Klauke <Pepe@Peters-MacBook-Pro.local>
2017-12-04 21:10:45 +0100

    added option for working with metrics that need to be minimized

--- BlendingOptimizer.py
+++ BlendingOptimizer.py
@@ -1,16 +1,20 @@
 import numpy as np
 
 class BlendingOptimizer:
-    max = True
+    maximize = True
     weights = []
     metric = None
     score = None
     
-    def __init__(self, metric, max = True):    
+    def __init__(self, metric, maximize = True):    
         self.metric = metric
-        self.max = max
+        self.maximize = maximize
         
-    def train(self, predictions, real, step = 0.1, init_weights = None, warm_start = False):
+    def is_better_score(self, score_to_test, score):
+        cond = score_to_test > score
+        return cond if self.maximize else not cond
+        
+    def fit(self, predictions, real, step = 0.1, init_weights = None, warm_start = False):
         if (init_weights != None) and warm_start:
             print('Warning: When warm_start is used init_weights are ignored.')
         
@@ -26,22 +30,24 @@ class BlendingOptimizer:
             assert(len(init_weights) == le)
             weights = init_weights
 
-        score, best_score = -1, 0
-        while(best_score > score):
-            score = best_score
+        score = 0
+        best_score = score + self.maximize -0.5
+        
+        while self.is_better_score(best_score, score):
             best_score = self.metric(real, np.average(predictions, weights=weights, axis=0))
+            score = best_score
             best_index, best_step = -1, 0.0
             for j in range(le):
                 delta = np.array([(0 if k != j else step) for k in range(le)])
                 s = self.metric(real, np.average(predictions, weights=weights+delta, axis=0))
-                if(s > best_score):
+                if self.is_better_score(s, best_score):
                     best_index, best_score, best_step = j, s, step
                     continue
                 if weights[j] - step >= 0:
                     s = self.metric(real, np.average(predictions, weights=weights-delta, axis=0))
                     if(s > best_score):
                         best_index, best_score, best_step = j, s, -step
-            if best_score > score:
+            if self.is_better_score(best_score, score):
                 weights[best_index] += best_step
                 
         self.weights = weights
