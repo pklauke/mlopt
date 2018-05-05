a6659fa7ee45a0e79a8ec8c81982b2359737968f
Peter Klauke <peter.klauke@tu-dortmund.de>
2018-05-05 16:38:29 +0200

    Package project

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ensemble',
+      version='0.1',
+      description='Package for ensembling Machine Learning models.',
+      url='https://github.com/pklauke/Ensemble',
+      author='Peter Klauke',
+      packages=['ensemble'],
+      zip_safe=False)
